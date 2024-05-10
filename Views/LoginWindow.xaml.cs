5dbbcd58c6663b2bd3c2453292f6cfae12df3297
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2024-05-10 14:59:38 +0300

    Added UI library

---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using Wpf.Ui.Controls;
+
+namespace Supermarket.Views;
+
+public partial class LoginWindow : FluentWindow
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
